d4f0bda5871d2495d4edc13aedfd7ad3774569f7
root <www.ppc007logs@gmail.com>
2019-04-15 14:02:11 +0530

    Dataset Modification

--- tests/test1.py
+++ tests/test1.py
@@ -1,13 +1,14 @@
 import os
 import cv2
 import numpy as np
-#from imutils import face_utils
+import dlib
+from imutils import face_utils
 import pickle
 from sklearn.preprocessing import LabelEncoder
 from augment.image import ImageAugmentation
 from augment.tweak import crop,resize,rotate_by_angle
 import tensorflow as tf
-#import mtcnn.mtcnn as MTCNN
+import mtcnn.mtcnn as MTCNN
 from recog_model.train import InceptionResnetV2ImageClassifier
 #from recog_model.inference import ImageInferenceEngine
 
@@ -21,18 +22,25 @@ labelencoder_y_1 = LabelEncoder()
 net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
 
 usr_map =  {}
-"""
+
 def detect_faces(img):
     (h, w) = img.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(img, (300,300)), 1.0, (300, 300), (103.93, 116.77, 123.68))
     net.setInput(blob)
     detections = net.forward()
-    confidence = detections[0, 0, 0, 2]
-    if confidence > 0.5:
-        x1 = int(detections[0, 0, 0, 3] * w)
-        y1 = int(detections[0, 0, 0, 4] * h)
-        x2 = int(detections[0, 0, 0, 5] * w)
-        y2 = int(detections[0, 0, 0, 6] * h)
+    max = 0
+    ind = -1
+    for i in range(0,detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5 and confidence > max:
+            ind = i
+            max = confidence
+    if ind != -1:
+        x1 = int(detections[0, 0, ind, 3] * w)
+        y1 = int(detections[0, 0, ind, 4] * h)
+        x2 = int(detections[0, 0, ind, 5] * w)
+        y2 = int(detections[0, 0, ind, 6] * h)
+
         predictor = dlib.shape_predictor("shape_predictor_5_face_landmarks.dat")
         rect = dlib.rectangle(x1,y1,x2,y2)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -61,17 +69,18 @@ def generate_augmeted_images():
         for train_img in os.listdir(celeb_train):
             img_dir = os.path.join(celeb_train,train_img)
             #d = o_mtcnn.detect_faces(cv2.imread(img_dir))
-            d = detect_faces(cv2.imread(img_dir))
+            img = cv2.imread(img_dir)
+            d = detect_faces(img)
             bbox = d[0]['box']
             keypoints = d[0]['keypoints']
             dY = keypoints['right_eye'][1] - keypoints['left_eye'][1]
             dX = keypoints['right_eye'][0] - keypoints['left_eye'][0]
             angle = np.degrees(np.arctan2(dY, dX)) - 180
-            img = cv2.imread(img_dir)
             img = rotate_by_angle(img,angle)
             width = bbox[2]
             height = bbox[3]
             f_img = resize(crop(img,[width,height],cords=(bbox[0],bbox[1])),(150,150))
+            cv2.imwrite(img_dir,f_img)
             tr_imgs.append(f_img)
             tr_labels.append(celebs)
             augmentation = ImageAugmentation(f_img)
@@ -80,11 +89,20 @@ def generate_augmeted_images():
                 for img in imgs:
                     tr_imgs.append(img[0])
                     tr_labels.append(celebs)
-                    cv2.imwrite(img_dir[:-4]+"_"+img[1]+".jpg",img[0])
+                    #cv2.imwrite(img_dir[:-4]+"_"+img[1]+".jpg",img[0])
     print("LOADED....TRAINING SET")
     tr_labels = labelencoder_y_1.fit_transform(tr_labels)
     return (tr_imgs,tr_labels,celebs)
 
+def normalize_images():
+    for celeb in os.listdir(train_dir):
+        celeb_dir = os.path.join(train_dir,celeb)
+        for train_img in os.listdir(celeb_dir):
+            img_dir = os.path.join(celeb_dir,train_img)
+            x = cv2.imread(img_dir)
+            y = np.zeros((150,150))
+            z = cv2.normalize(x,y,0,255,norm_type=cv2.NORM_MINMAX)
+            cv2.imwrite(img_dir,z)
 
 def load_validation_set():
     #o_mtcnn = MTCNN.MTCNN()
@@ -111,14 +129,18 @@ def load_validation_set():
     print("LOADED...VALIDATION SET")
     val_labels = labelencoder_y_1.fit_transform(val_labels)
     return (val_imgs,val_labels)
-"""
+
 
 def test():
     tf.logging.set_verbosity(tf.logging.ERROR)
     celebs = 5
+    #generate_augmeted_images()
+    normalize_images()
     tr_imgs,tr_labels = None,None
     val_imgs,val_labels = None,None
+
     #print("LOADING....InceptionResnetV2ImageClassifier")
+    """
     print("LOADING DATASETS")
     with open("dataset/tr_imgs.pkl","rb") as f :
         tr_imgs = np.array(pickle.load(f))
@@ -128,7 +150,8 @@ def test():
         val_imgs = np.array(pickle.load(f))
     with open("dataset/var_labels.pkl","rb") as f :
         val_labels = np.array(pickle.load(f))
-    """with open("dataset/tr_imgs.pkl","wb") as f :
+
+    with open("dataset/tr_imgs.pkl","wb") as f :
         pickle.dump(tr_imgs,f)
     with open("dataset/tr_labels.pkl","wb") as f :
         pickle.dump(tr_labels,f)
@@ -139,8 +162,8 @@ def test():
 
     print("LOADED DATASETS")
     print("LOADING....InceptionResnetV2ImageClassifier")
-    irv2 = InceptionResnetV2ImageClassifier(model_asset="Face_Recognizer")
+    #irv2 = InceptionResnetV2ImageClassifier(model_asset="Face_Recognizer")
     print("LOADING....TRAINING AND VALIDATION SETS")
-    irv2.define_data(tr_img_data=np.array(tr_imgs),tr_labels=np.array(tr_labels),classes=celebs,val_img_data=np.array(val_imgs),val_labels=np.array(val_labels))
+    #irv2.define_data(tr_img_data=np.array(tr_imgs),tr_labels=np.array(tr_labels),classes=celebs,val_img_data=np.array(val_imgs),val_labels=np.array(val_labels))
     print("LOADING....FITTING IRV2 MODEL")
-    irv2.fit_model()
+    #irv2.fit_model()
